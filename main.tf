f632a5b14db5de64a119f571392b4919214b159f
solomon <sokhifoh@yahoo.com>
2023-09-30 19:05:09 -0600

    another FMT

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "my_test_vpc" {
-  cidr_block = "10.0.0.0/16"
-}
+#resource "aws_vpc" "my_test_vpc" {
+  #cidr_block = "10.0.0.0/16"
+#}
