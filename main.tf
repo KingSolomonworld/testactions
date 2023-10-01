13e94eccd2463842df06acc4b2e78c6adfad6ce3
solomon <sokhifoh@yahoo.com>
2023-09-30 19:37:23 -0600

    keys

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-1"
 }
 
-#resource "aws_vpc" "my_test_vpc" {
-#cidr_block = "10.0.0.0/16"
-#}
+resource "aws_vpc" "my_test_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
